b2290f722722030801fc76f4bfe3e6fd3555f033
ranganathnk <ranganathnkulkarni@gmail.com>
2022-01-25 11:57:59 -0800

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import React ,{ useEffect,useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import {setUserData} from '../actions/userActions'
-import { makeStyles } from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,6 +9,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 
 function createData(name, calories, fat, carbs, protein) {
@@ -46,7 +47,7 @@ const Home=()=>{
     },[])
     return(
     <div className="container">
-        
+      <h3 style={{align:"center"}}>Users List</h3>  
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
@@ -55,6 +56,7 @@ const Home=()=>{
             <TableCell align="center">Name</TableCell>
             <TableCell align="center">Email</TableCell>
             <TableCell align="center">Contact</TableCell>
+            <TableCell>Action</TableCell>
             {/* <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
           </TableRow>
         </TableHead>
@@ -64,13 +66,21 @@ const Home=()=>{
               key={userr.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-               {/* <TableCell component="th" scope="row">
-                {userr.id}
-              </TableCell>  */}
-                <TableCell align="center">{userr.id}</TableCell> 
-               <TableCell align="center">{userr.name}</TableCell>
-              <TableCell align="center">{userr.email}</TableCell>
-              <TableCell align="center">{userr.contact}</TableCell>
+                    {/* <TableCell component="th" scope="row">
+                        {userr.id}
+                    </TableCell>  */}
+                        <TableCell align="center">{userr.id}</TableCell> 
+                    <TableCell align="center">{userr.name}</TableCell>
+                    <TableCell align="center">{userr.email}</TableCell>
+                    <TableCell align="center">{userr.contact}</TableCell>
+                    <TableCell align="center">   
+                        <Stack spacing={2} direction="row">
+                            <Button variant="contained">Update</Button>
+                            <Button variant="contained">Delete</Button>
+                            {/* <Button variant="outlined">Outlined</Button> */}
+                        </Stack>
+                    </TableCell>
+
             </TableRow>
           ))}
         </TableBody>
