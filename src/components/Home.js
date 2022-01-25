bb9cea1047e5c66da7692bdb3df677d65632f4c5
ranganathnk <ranganathnkulkarni@gmail.com>
2022-01-25 11:24:46 -0800

    MUI TABLE

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,27 @@ import React ,{ useEffect,useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import {setUserData} from '../actions/userActions'
+import { makeStyles } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
 
 const Home=()=>{
     const allUsers=useSelector((state)=>state.userDataHere);
@@ -26,19 +46,36 @@ const Home=()=>{
     },[])
     return(
     <div className="container">
-      <table border="2">
-          <tr>
-              <th>Name</th>
-          </tr>
-          
-                {allUsers.map((userr)=>(
-            <tr>
-                <td>{userr.name} </td>
-            </tr>
-                ) )
-                }
-         
-    </table>
+        
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">ID</TableCell>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">Contact</TableCell>
+            {/* <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {allUsers.map((userr) => (
+            <TableRow
+              key={userr.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+               {/* <TableCell component="th" scope="row">
+                {userr.id}
+              </TableCell>  */}
+                <TableCell align="center">{userr.id}</TableCell> 
+               <TableCell align="center">{userr.name}</TableCell>
+              <TableCell align="center">{userr.email}</TableCell>
+              <TableCell align="center">{userr.contact}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
 
     );
